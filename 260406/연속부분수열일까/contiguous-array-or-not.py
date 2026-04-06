856e5ecf1f76e408d99e6176a58b224c11e04862
kwr243418 <kwr243418@mokpo.ac.kr>
2026-04-06 12:31:09 +0900

    Title: 연속부분수열일까, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260406/연속부분수열일까/contiguous-array-or-not.py
+++ 260406/연속부분수열일까/contiguous-array-or-not.py
@@ -4,14 +4,14 @@ B = list(map(int, input().split()))
 count = 0
 if B[0] in A:
     idx = A.index(B[0])
-    for i in range(1, len(B)):
+    for i in range(1, N2):
         if A[idx + i] == B[i]:
             count += 1
         else :
             break
 else :
     print("No")
-if count == len(B) - 1:
+if count == N2 - 1:
     print("Yes")
 else :
     print("No")
