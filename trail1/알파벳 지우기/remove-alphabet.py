1267df353034ed8c9a05fe45575396bd54c74a2d
kwr243418 <kwr243418@mokpo.ac.kr>
2026-06-08 11:41:37 +0900

    [Passed] 알파벳 지우기 (53ms, 16MB)

---
+++ trail1/알파벳 지우기/remove-alphabet.py
@@ -0,0 +1,13 @@
+A = input()
+B = input()
+A2, B2 = '', ''
+for i in A:
+    if i.isdigit():
+        A2 += i
+
+for i in B:
+    if i.isdigit():
+        B2 += i
+
+sum = int(A2) + int(B2)
+print(sum)
