b5a28972a7e3b6f4a057b5a602aa7aac3af349b5
kwr243418 <kwr243418@mokpo.ac.kr>
2026-07-02 17:00:32 +0900

    [Passed] 숫자로 이루어진 사각형 (54ms, 16MB)

---
+++ trail2/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+# Please write your code here.
+def nSquare(n):
+    num = 1
+    for _ in range(n):
+        for _ in range(n):
+            if num == 10:
+                num = 1
+            print(num, end=' ')
+            num += 1
+        print()
+
+
+nSquare(n)
